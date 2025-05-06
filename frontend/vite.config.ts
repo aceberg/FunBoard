b06afe1879c1767e72ed105e56f1693228a24351
aceberg <1502200+aceberg@users.noreply.github.com>
2025-05-06 23:25:15 +0700

    logo

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid(), 
+    tailwindcss(),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
